467f9d5282b09003771e7cad0d57e0d92f00763e
Artem Bychkov <artem.b91@mail.ru>
2022-08-23 18:05:47 +0300

    refactor: small fixes

--- src/main/java/hexlet/code/controllers/UrlCheckController.java
+++ src/main/java/hexlet/code/controllers/UrlCheckController.java
@@ -6,6 +6,7 @@ import hexlet.code.domain.Url;
 import hexlet.code.domain.UrlCheck;
 import hexlet.code.domain.query.QUrl;
 import io.javalin.http.Handler;
+import kong.unirest.UnirestException;
 
 public final class UrlCheckController {
 
@@ -25,7 +26,20 @@ public final class UrlCheckController {
             return;
         }
 
-        UrlCheck urlCheck = UrlChecker.check(url);
+        UrlCheck urlCheck;
+        try {
+            urlCheck = UrlChecker.check(url);
+        } catch (RuntimeException e) {
+            if (e instanceof UnirestException) {
+                ctx.sessionAttribute("error", "Ошибка при подключении к сайту. Повторите запрос позднее");
+                // Устанавливать 504 ошибку не стал, т.к. в итоге редирект ее затрет
+            } else {
+                ctx.sessionAttribute("error", "Внутренняя ошибка сервера");
+            }
+            ctx.redirect("/urls/" + urlId);
+            return;
+        }
+
         urlCheck.save();
 
         ctx.sessionAttribute("flash", "Страница успешно проверена");
